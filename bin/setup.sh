3872cea3dc8baefa201c0e668ef85f90d22bacc4
voboshi <22003158+voboshi@users.noreply.github.com>
2023-07-09 16:18:35 +0100

    feat: clean-up

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,5 +8,5 @@ export PATH="$HOME/Library/Python/3.9/bin:/opt/homebrew/bin:$PATH"
 
 sudo pip3 install --upgrade pip
 
-pip3 install ansible
+pip3 install --user ansible
 ansible-galaxy install -r requirements.yml
